e819dac5982611272498ba5872c35a27f15e2216
MotoClique <40442195+MotoClique@users.noreply.github.com>
2018-08-09 18:06:19 +0530

    Custom SnackBar

---
+++ src/app/customSnackBar.component.js
@@ -0,0 +1,42 @@
+//SnackBar Component
+import {Component, Inject} from '@angular/core';
+import {MAT_SNACK_BAR_DATA, MatSnackBarRef} from '@angular/material';
+
+
+//For Success Message
+@Component({
+  //selector: '',
+  template: '<div class="successSnackBar"><span class="glyphicon glyphicon-ok"></span> {{data.message}}</div>',
+  styles: ['.successSnackBar { color: #00FF06; }'],
+})
+export class SuccessSnackBarComponent {
+	 constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any, private snackBarRef: MatSnackBarRef<SuccessSnackBarComponent>) { }
+}
+
+
+
+//For Error Message
+@Component({
+  //selector: '',
+  template: '<div class="errorSnackBar"><span class="glyphicon glyphicon-alert"></span> {{data.message}}</div> <div *ngIf="data.action" style="display:flex; justify-content:flex-end;"><button mat-button color="accent" (click)="onActionClick($event)">{{data.action}}</button></div>',
+  styles: ['.errorSnackBar { color: #FF5757; }'],
+})
+export class ErrorSnackBarComponent {
+	 constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any,  private snackBarRef: MatSnackBarRef<ErrorSnackBarComponent>) { }
+	 
+	 onActionClick(evt){
+		this.snackBarRef.dismissWithAction();
+	 }
+}
+
+
+
+//For Information Message
+@Component({
+  //selector: '',
+  template: '<div class="infoSnackBar"><span class="glyphicon glyphicon-info-sign"></span> {{data.message}}</div>',
+  styles: ['.infoSnackBar { color: #F3F3F3; }'],
+})
+export class InfoSnackBarComponent {
+	 constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any,  private snackBarRef: MatSnackBarRef<InfoSnackBarComponent>) { }
+}
