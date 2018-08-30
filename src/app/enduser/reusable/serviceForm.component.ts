bf38d21e1589aa55edd8b2bca924f1ec56ecaf9f
MotoClique <40442195+MotoClique@users.noreply.github.com>
2018-08-30 18:12:02 +0530

    Thumbs count manual update

--- src/app/enduser/reusable/serviceForm.component.ts
+++ src/app/enduser/reusable/serviceForm.component.ts
@@ -307,7 +307,10 @@ export class AppServiceForm implements OnInit {
 					   //debugger;
 						if(data.statusCode=="S"){
 							this.parentComponent.openItem(this.serviceItem);
-              feedback.thumbs_up_no = feedback.thumbs_up_no - (-1);
+              if(isNaN(feedback.thumbs_up_no))
+                feedback.thumbs_up_no = '1';
+              else
+                feedback.thumbs_up_no = feedback.thumbs_up_no - (-1);
 						}
 						else{
 							this.sharedService.openMessageBox("E","Unable to Thumbs Up.",null);
@@ -331,7 +334,10 @@ export class AppServiceForm implements OnInit {
 						   //debugger;
 							if(data.statusCode=="S"){
 								this.parentComponent.openItem(this.serviceItem);
-                feedback.thumbs_down_no = feedback.thumbs_down_no - (-1);
+                if(isNaN(feedback.thumbs_down_no))
+                  feedback.thumbs_down_no = '1';
+                else
+                  feedback.thumbs_down_no = feedback.thumbs_down_no - (-1);
 							}
 							else{
 								this.sharedService.openMessageBox("E","Unable to Thumbs Down.",null);
