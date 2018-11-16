fdf809e07b904fc0851699ac62e1f0fcd51971c7
MotoClique123 <motoclique123@gmail.com>
2018-11-17 01:40:03 +0530

    chats for deleted post

--- src/app/enduser/chatDetail.component.ts
+++ src/app/enduser/chatDetail.component.ts
@@ -46,7 +46,7 @@ export class AppChatDetail implements OnInit {
     ngOnInit(){
 				var that = this;
 				this.sharedService.sharedObj.containerContext.title = "My Chat";
-				if(!(this.sharedService.sharedObj.postItem.user_id))
+				if(!(this.sharedService.sharedObj.postItem.from_user))
 					this.router.navigateByUrl('/Container/ChatInbox');
 				this.sharedService.getUserProfile(function(user){
 					that.userDetail = user;
