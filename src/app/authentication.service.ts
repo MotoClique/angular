9ab83f80d4491c5a9e3283397efe4311aca8c2f4
MotoClique <40442195+MotoClique@users.noreply.github.com>
2018-12-03 21:45:16 +0530

    url for new gcp prod server

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -22,7 +22,7 @@ export class AuthenticationService {
 	var prdEnvFlag = true;
 	  
 	window.GlobalData = window.GlobalData || {};
-	window.GlobalData['MainUrlDomain'] = (prdEnvFlag)?"https://motoclique.herokuapp.com":"https://meanmav.herokuapp.com";
+	window.GlobalData['MainUrlDomain'] = (prdEnvFlag)?(this.protocol+"//"+ this.host):"https://meanmav.herokuapp.com";
 	  
 	if(this.hostname === 'localhost')
 		this.fullhost = window.GlobalData['MainUrlDomain']; //"http://nodemav-nodemav.7e14.starter-us-west-2.openshiftapps.com";
