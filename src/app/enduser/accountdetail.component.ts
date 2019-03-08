024e3372341758f75140365f90a7488260eca583
MotoClique <40442195+MotoClique@users.noreply.github.com>
2019-03-08 16:34:30 +0530

    dob issue

--- src/app/enduser/accountdetail.component.ts
+++ src/app/enduser/accountdetail.component.ts
@@ -66,7 +66,7 @@ export class AppAccountDetail implements OnInit {
 		this.disabledMode = true;
 		this.sharedService.getUserProfile(function(user){
 			that.userDetail = user;
-      if(user.dob){
+      if(user.dob && typeof user.dob === 'string'){
         var dateString:any = (user.dob).split('/'); dateString = dateString[2]+'-'+dateString[1]+'-'+dateString[0];
         that.userDetail.dob = new Date(dateString);
       }
@@ -85,7 +85,7 @@ export class AppAccountDetail implements OnInit {
 				}
 				else if(details.results && details.results.length > 0){
 					this.userDetail = details.results[0];
-          if(this.userDetail.dob){
+          if(this.userDetail.dob && typeof this.userDetail.dob === 'string'){
             var dateString:any = (this.userDetail.dob).split('/'); dateString = dateString[2]+'-'+dateString[1]+'-'+dateString[0];
             this.userDetail.dob = new Date(dateString);
           }
