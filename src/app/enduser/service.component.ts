8eedd2dcdcffbf5128aa786141bdd7d61fc0424d
MotoClique <40442195+MotoClique@users.noreply.github.com>
2019-04-11 14:01:59 +0530

    buffer data

--- src/app/enduser/service.component.ts
+++ src/app/enduser/service.component.ts
@@ -109,6 +109,29 @@ export class AppService implements OnInit {
 						var id = that.route.snapshot.params.id;
 						var mode = that.route.snapshot.params.mode;
 						if(id && id !== 'blank' && id !== 'new'){
+              if(that.sharedService.sharedObj.postItem && that.sharedService.sharedObj.postItem.service_id == id){
+									var item = jQuery.extend(true, {}, that.sharedService.sharedObj.postItem);
+									that.sharedService.sharedObj.postItem = {};
+									that.item = item;
+									that.item.transactionTyp = "Service";
+									that.dynamicFormComponent.item = that.item;
+									that.imageTemplateComponent.item = that.item;
+									that.imageTemplateComponent.postImage = jQuery.extend(true, {}, that.sharedService.sharedObj.postImage);
+									that.sharedService.sharedObj.postImage = {};
+									if(mode === 'edit'){
+										that.goEditMode();
+									}else{
+										that.detail = true;
+										that.editMode = false;
+										that.screenMode = {add:false, edit:false};
+										that.serviceFormComponent.loadService(item);
+										that.imageTemplateComponent.getTransactionThumbnails(item.service_id);
+									}
+                    
+                    if(that.item.user_id !== that.userDetail.user_id)
+										  that.sharedService.sharedObj.containerContext.title = "Services";
+							}
+							else{
 							that.commonService.enduserService.getService("",id,"",null,null,null)
 							  .subscribe( data => {
 								  if(data.results.length > 0){
@@ -134,6 +157,7 @@ export class AppService implements OnInit {
 									  that.sharedService.openMessageBox("E","No data found.",null);
 								  }
 								});
+              }
 						}
             else if(id === 'new'){
 							that.createNew(null);
